25324cd44a6ed78b114b4fff3f0f6b9492d47b3d
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-11-14 11:38:01 +0100

    added Projects res. literal, return issues for a project

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,43 @@ import APIService from "./sentry";
 const app: Express = express();
 const port: number = 9000;
 
-const apiService = new APIService("6d8aec61cda648fe9d91eda369cc747071936790f0e54bf181ce7c91417e390e");
+const apiService = new APIService(process.env.SENTRY_API_KEY || "6d8aec61cda648fe9d91eda369cc747071936790f0e54bf181ce7c91417e390e");
 
 app.get("/", async (req, res) => {
     res.send("Hey Hooman!");
 });
 
-app.get("/api/issues", async (req: Request, res: Response) => {
-    const allProjects: string[] = await apiService.allProjects();
-    console.log(allProjects);
+app.get("/api/orgs", async (req: Request, res: Response) => {
+    const organization: string = await apiService.getOrgName();
+    return res.status(200).json({
+        data: {
+            org_name: organization,
+        },
+    });
+});
+
+app.get("/api/projects", async (req: Request, res: Response) => {
+    const allProjects = await apiService.allProjects();
     return res.status(200).json({ data: allProjects });
 });
 
-app.get("/api/orgs", async (req: Request, res: Response) => {
-    let organization: string = await apiService.getOrgName()
-    return res.status(200).json({
-        data: {
-            org_name: organization
+app.get("/api/:org/:projectName/issues", async (req: Request, res: Response) => {
+    try {
+        const orgName = req.params.org ? req.params.org : null;
+        const projectName = req.params.projectName ? req.params.projectName : null;
+
+        // pass to the sentryAPI service method
+        const projectIssues = await apiService.projectIssues(orgName, projectName);
+        return res.status(200).json({ data: projectIssues });
+    } catch (e) {
+
+        if (e === "ENOTFOUND") {
+            return res.status(404).json({
+                data: "ENOTFOUND",
+            });
         }
-    })
-})
+    }
+});
 
 app.listen(port, () => {
     console.log(`APP UP AND RUNNING ON PORT ${port}`);
--- src/sentry.ts
+++ src/sentry.ts
@@ -1,5 +1,12 @@
 import Axios, { AxiosRequestConfig } from "axios";
 
+interface IProjectsObjectLiteral {
+    platform: string;
+    slug: string;
+    status: string;
+    orgSlug: string;
+    id: string;
+}
 class APIService {
     private authToken: string;
     private baseURL: string;
@@ -17,21 +24,51 @@ class APIService {
         return name;
     }
     // allPprojects method gets all the projects available for the org
-    public async allProjects(): Promise<string[]> {
+    public async allProjects(): Promise<IProjectsObjectLiteral[]> {
         try {
             // make request to sentry
             const { data } = await Axios.get(`${this.baseURL}/projects/`, this.axiosConfig());
-
-            const projectsNameArray: string[] = [];
+            const projectsNameArray: IProjectsObjectLiteral[] = [];
 
             for (const project of data) {
-                const { slug } = project;
-                projectsNameArray.push(slug);
+                const { slug, status, platform, organization: { slug: projectOrgSlug }, id } = project;
+                const projectObject = {
+                    platform,
+                    slug,
+                    status,
+                    orgSlug: projectOrgSlug,
+                    id,
+                };
+                projectsNameArray.push(projectObject);
             }
             return projectsNameArray;
         } catch (e) {
-            console.log(e);
+            // console.log(e);
+            if (e?.response?.status === 404) {
+                console.log(`ENOFOUND`, e.response);
+                return e.response.status;
+            }
             return e;
+        }
+    }
+
+    // list all the issues in the org
+    // TODO: Add support for period query.
+    public async projectIssues(orgName: string, projectName: string): Promise<object> {
+        try {
+            // first, get the projec. either by name or by ID
+            const bgOrgName = orgName ? orgName : await this.getOrgName();
+            const bgProjects = await this.allProjects();
+            const bgProjectName = projectName ? projectName : bgProjects[0].slug;
+            const { data } = await Axios.get(`${this.baseURL}/api/${bgOrgName}/${bgProjectName}/issues/`);
+
+            return data.data;
+        } catch (e) {
+            // console.log(e);
+            if (e?.response?.status === 404) {
+                return e.response;
+            }
+            throw new Error("EINTSERVER");
         }
     }
 
